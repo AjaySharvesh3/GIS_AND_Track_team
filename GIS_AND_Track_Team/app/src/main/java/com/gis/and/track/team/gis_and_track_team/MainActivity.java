0ac35bead88f0276e2d57216fba3fbaebbca9d78
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2018-10-05 14:33:45 +0530

    Initial starter code for GIS_AND_Track_Team

---
+++ GIS_AND_Track_Team/app/src/main/java/com/gis/and/track/team/gis_and_track_team/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gis.and.track.team.gis_and_track_team;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
